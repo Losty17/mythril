1f52045da998a100fe21c475ee17f314f56c0f26
Vinícius Kappke <viniciuskappke@gmail.com>
2022-08-07 01:07:39 -0300

    add redirects to login

--- src/pages/Board/index.tsx
+++ src/pages/Board/index.tsx
@@ -9,20 +9,23 @@ const Board: React.FC<BoardProps> = () => {
   const user = useAppSelector((state) => state.user.value);
 
   useEffect(() => {
-    if (!user) navigate("/");
+    if (!user) navigate("/login");
   }, [user]);
 
   return (
-    <div className="grid-dashboard-container min-h-screen">
-      <div className="p-3 flex flex-row">
-        <Logo hideName className="h-7 mr-2.5" dividerMargin="mx-3" />
-        <span className="text-2.5xl leading-7 font-display text-mythril-100">
-          Welcome, {user?.name}!
-        </span>
-      </div>
-
-      <GridDashboard />
-    </div>
+    <>
+      {user && (
+        <div className="grid-dashboard-container min-h-screen">
+          <div className="p-3 flex flex-row">
+            <Logo hideName className="h-7 mr-2.5" dividerMargin="mx-3" />
+            <span className="text-2.5xl leading-7 font-display text-mythril-100">
+              Welcome, {user?.name}!
+            </span>
+          </div>
+          <GridDashboard />
+        </div>
+      )}
+    </>
   );
 };
 
